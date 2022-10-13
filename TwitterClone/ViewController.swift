a3f06129b84d276fb0b4cf1316b46f2a70fdf1c3
utkubalkir <devucb01@gmail.com>
2022-10-14 01:47:46 +0300

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Utku Can BALKIR on 14.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
